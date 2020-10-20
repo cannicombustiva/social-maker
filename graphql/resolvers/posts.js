632feb1e0ef0a7407a8768973319e88e757cb3e5
Salvatore Difranco <salvatoredifranco8@gmail.com>
2020-10-20 17:16:22 +0200

    feat: added deletePost feature

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,5 +1,7 @@
 const Post = require('../../models/Post');
-const checkAuth = require('../../util/check-auth')
+const checkAuth = require('../../util/check-auth');
+const { AuthenticationError } = require('apollo-server');
+
 
 module.exports = {
     Query: {
@@ -41,6 +43,21 @@ module.exports = {
             const post = await newPost.save();
 
             return post;
+        },
+        async deletePost(_, { postId }, context){
+            const user = checkAuth(context);
+
+            try{
+                const post = await Post.findById(postId);
+                if(user.username === post.username){
+                    await post.delete();
+                    return 'Post deleted successfully';
+                } else {
+                    throw new AuthenticationError('Action not allowed');
+                }
+            }catch(err){
+                throw new Error(err);
+            }
         }
     }
 }
