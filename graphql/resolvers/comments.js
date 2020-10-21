7a1ccef48d5551f7b3ec124b72e062165a8f1209
Salvatore Difranco <salvatoredifranco8@gmail.com>
2020-10-21 18:18:55 +0200

    feat: added delete comment

--- graphql/resolvers/comments.js
+++ graphql/resolvers/comments.js
@@ -1,11 +1,11 @@
-const { UserInputError } = require('apollo-server');
+const { UserInputError, AuthenticationError } = require('apollo-server');
 const Post = require('../../models/Post');
 const checkAuth = require('../../util/check-auth');
 
 module.exports = {
     Mutation: {
         createComment: async (_, { postId, body }, context) => {
-            const { username} = checkAuth(context);
+            const { username } = checkAuth(context);
             if(body.trim() === ""){
                 throw new UserInputError('Empty comment', {
                     errors: {
@@ -25,6 +25,23 @@ module.exports = {
                 await post.save();
                 return post;
             } else throw new UserInputError('Post not found');
+        },
+        async deleteComment(_, { postId, commentId }, context){
+            const { username } = checkAuth(context);
+
+            const post = await Post.findById(postId);
+
+            if(post){
+                const commentIndex = post.comments.findIndex(c => c.id === commentId);
+
+                if(post.comments[commentIndex].username === username){
+                    post.comments.splice(commentIndex, 1);
+                    await post.save();
+                    return post;
+                } else {
+                    throw new AuthenticationError('Action not allowed');
+                }
+            } else new UserInputError('Post not found');
         }
     }
 }
