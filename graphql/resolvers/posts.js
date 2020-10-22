3c7f0cb688b651288bc56536eb98bb1d350fe98d
Salvatore Difranco <salvatoredifranco8@gmail.com>
2020-10-22 09:43:01 +0200

    feat: added Error about empty post

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,6 +1,7 @@
 const Post = require('../../models/Post');
 const checkAuth = require('../../util/check-auth');
 const { AuthenticationError, UserInputError } = require('apollo-server');
+const { argsToArgsConfig } = require('graphql/type/definition');
 
 
 module.exports = {
@@ -31,7 +32,10 @@ module.exports = {
     Mutation: {
         async createPost(_, { body }, context){
             const user = checkAuth(context);
-            console.log(user);
+
+            if(argsToArgsConfig.body.trim() === '') {
+                throw new Error('Post not be empty');
+            }
 
             const newPost = new Post({
                 body,
